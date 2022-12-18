bdff9d5611f6ed248afb890f2cc2a62ef43c028d
kuzznicki <kuznicki@protonmail.com>
2022-12-18 10:13:12 +0100

    prepare to deploy on gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/scss/bootstrap.scss';
 import '@/styles/main.scss'
 
@@ -11,9 +11,9 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 )
