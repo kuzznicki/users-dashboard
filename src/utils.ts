88dfd2204ab869711925e5fe747d52bab989673f
kuzznicki <kuznicki@protonmail.com>
2022-12-18 10:10:01 +0100

    add dist

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function capitalize(str: string): string {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-export const notAlphanumericOrSpaceRegex = /[^a-zA-Z0-9 ]/g;
-
-export function generateUsername(name: string) {
-    return name.trim().replace(notAlphanumericOrSpaceRegex, '').replaceAll(' ', '_').toLowerCase();
-}
